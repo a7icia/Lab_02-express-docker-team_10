067e913a10cf2c908c989a4eb42fc5bd7a9b0197
Zehra Ilgin <zehrai@hotmail.de>
2025-04-17 13:14:07 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const server = express();
+const port = 3001;
+
+
+server.get("/", function (req, res) {
+    res.send(`
+        <h1>Welcome!</h1>
+        <p>Visit the <a href="/hello">hello page</a>.</p>
+    `);
+});
+
+server.get("/hello", function (req, res) {
+    res.send("Hello IMIs!");
+});
+
+server.listen(port, function () {
+    console.log("Express listening on " + port);
+});
