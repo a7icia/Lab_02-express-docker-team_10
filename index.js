f9a18fb7c91ca6ce8870cd5fe25a6d2ea59fcb10
Duaax24 <167181641+Duaax24@users.noreply.github.com>
2025-04-30 21:59:40 +0200

    add changes

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require("express");
+const { engine } = require('express-handlebars');
 const path = require("path");
-const server = express();
+
+const app = express();
 const port = 3002;
 
-server.use(express.static(path.join(__dirname, "public_html")));
+// Handlebars einrichten
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views')); // wichtig!
 
-server.get("/", function (req, res) {
+// Statische Dateien bereitstellen
+app.use(express.static(path.join(__dirname, "public_html")));
+
+// Dummy-Daten
+const shelves = [
+  { name: 'Bookshelf', description: 'A shelf full of books' },
+];
+
+// Routen
+app.get("/", function (req, res) {
     res.send(`
         <h1>Welcome!</h1>
-        <p>Visit the <a href="/hello">hello page</a>.</p>
+        <p>Visit the <a href="/hello">hello page</a> or <a href="/shelf">the shelf page</a>.</p>
     `);
 });
 
-server.get("/hello", function (req, res) {
+app.get("/hello", function (req, res) {
     res.send("Hello IMIs!");
 });
 
-server.listen(port, function () {
+app.get("/shelf", (req, res) => {
+    res.render("shelf", { title: 'My Shelf Collection', shelves });
+});
+
+// Server starten
+app.listen(port, function () {
     console.log("Express listening on http://localhost:" + port);
 });
