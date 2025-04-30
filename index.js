c56aef86004a46dd8709a558e6dfb8c89227f418
Duaax24 <167181641+Duaax24@users.noreply.github.com>
2025-04-30 22:24:57 +0200

    add changes

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
 const express = require("express");
-const { engine } = require('express-handlebars');
+const { engine } = require("express-handlebars");
 const path = require("path");
 
 const app = express();
 const port = 3002;
 
-// Handlebars einrichten
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views')); // wichtig!
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-// Statische Dateien bereitstellen
+
 app.use(express.static(path.join(__dirname, "public_html")));
+app.use(express.urlencoded({ extended: true }));
 
-// Dummy-Daten
+
 const shelves = [
-  { name: 'Bookshelf', description: 'A shelf full of books' },
+  { name: "Bookshelf", description: "A shelf full of books" },
 ];
 
-// Routen
-app.get("/", function (req, res) {
-    res.send(`
-        <h1>Welcome!</h1>
-        <p>Visit the <a href="/hello">hello page</a> or <a href="/shelf">the shelf page</a>.</p>
-    `);
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Welcome!</h1>
+    <p>Visit the <a href="/hello">hello page</a> or <a href="/shelf">the shelf page</a>.</p>
+  `);
 });
 
-app.get("/hello", function (req, res) {
-    res.send("Hello IMIs!");
+app.get("/hello", (req, res) => {
+  res.send("Hello IMIs!");
 });
 
 app.get("/shelf", (req, res) => {
-    res.render("shelf", { title: 'My Shelf Collection', shelves });
+  res.render("shelf", {
+    layout: "main",
+    title: "My Shelf Collection",
+    shelves,
+  });
 });
 
-// Server starten
-app.listen(port, function () {
-    console.log("Express listening on http://localhost:" + port);
+app.get("/shelf/create", (req, res) => {
+  res.render("createShelf", {
+    layout: "main",
+    title: "Regal erstellen",
+  });
+});
+
+app.post("/shelf/create", (req, res) => {
+  const { name, location } = req.body;
+
+  
+  shelves.push({ name, description: `Standort: ${location}` });
+  res.redirect("/shelf");
+});
+
+
+app.listen(port, () => {
+  console.log("Express listening on http://localhost:" + port);
 });
