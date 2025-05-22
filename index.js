a9aa02e96ef0f9156bfb99c7df79a1322e2f64cc
Duaax24 <167181641+Duaax24@users.noreply.github.com>
2025-05-22 12:45:38 +0200

    add changes

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
 const path = require("path");
+const Handlebars = require("handlebars"); 
+
+Handlebars.registerHelper('array', function() {
+return Array.prototype.slice.call(arguments, 0, -1);
+});
+
+Handlebars.registerHelper('lte', function(a, b) {
+  return a <= b;
+});
 
 const app = express();
 const port = 3002;
@@ -15,40 +24,70 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const shelves = [
-  { name: "Bookshelf", description: "A shelf full of books" },
+  { name: "Bookshelf", description: "A shelf full of books" },
 ];
+
+
+const readBooks = [
+  { title: "The complete Developer", author: "Martin Krause", rating: 5  },
+  { title: "Basiswissen Informatik", author: "Eckert Zitzler", rating: 3 },
+  { title: "Harry Potter und der Stein der Weisen", author: "J. K. Rowling", rating: 4 },
+];
+
+
+app.get('/shelf/read', (req, res) => {
+  res.render('shelf_read', { 
+    layout: "main",
+    title: "Gelesene Bücher",
+    books: readBooks 
+  });
+});
+
+
+app.post('/shelf/read/add', (req, res) => {
+  const { title, author } = req.body;
+  if (title && author) {
+    readBooks.push({
+      title,
+      author,
+      rating: 3 
+    });
+  }
+  res.redirect('/shelf/read');
+});
+
 
 
 app.get("/", (req, res) => {
  
-  res.render("lists", {
-    layout: "main",
-    title: "Meine Bücherregale",
-    shelves,
-  });
+  res.render("lists", {
+    layout: "main",
+    title: "Meine Bücherregale",
+    shelves,
+  });
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello IMIs!");
+  res.send("Hello IMIs!");
 });
 
 
 
 app.get("/shelf/create", (req, res) => {
-  res.render("createShelf", {
-    layout: "main",
-    title: "Regal erstellen",
-  });
+  res.render("createShelf", {
+    layout: "main",
+    title: "Regal erstellen",
+  });
 });
 app.post("/shelf/create", (req, res) => {
-  const { name, description } = req.body;
+  const { name, description } = req.body;
 
-  
-  shelves.push({ name, description });
-  res.redirect("/");
+  
+  shelves.push({ name, description });
+  res.redirect("/");
 });
 
 
 app.listen(port, () => {
-  console.log("Express listening on http://localhost:" + port);
-});
+  console.log("Express listening on http://localhost:" + port);
+}); 
